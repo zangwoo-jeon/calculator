2b2dea9d6e8fedd09bac302c6096d6fc9e40d954
DESKTOP-Q68S0MK\zangw <create7827@naver.com>
2024-08-05 17:50:42 +0900

    Add test edit te1 and fix btn1 handler function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 class Calculator(QWidget):
 
@@ -8,10 +8,14 @@ class Calculator(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
 
         vbox = QVBoxLayout()
+        vbox.addWidget(self.te1)
         vbox.addStretch(1)
         vbox.addWidget(self.btn1)
         vbox.addStretch(1)
@@ -24,7 +28,8 @@ class Calculator(QWidget):
         self.show()
 
     def activateMessage(self):
-        QMessageBox.information(self, "information", "Button clicked!")
+        # QMessageBox.information(self, "information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
