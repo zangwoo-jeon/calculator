4317db8401c8f8f9cf1b6ee996c7a8ec8663f143
DESKTOP-Q68S0MK\zangw <create7827@naver.com>
2024-08-11 21:50:41 +0900

    Modify activateMessage function to add an argument

--- ui.py
+++ ui.py
@@ -50,8 +50,8 @@ class View(QWidget):
         self.resize(256, 256)
         self.show()
 
-    def activateMessage(self):
-        self.te1.appendPlainText("Button clicked!")
+    def activateMessage(self, text):
+        self.te1.appendPlainText(text)
 
     def clearMessage(self):
         self.te1.clear()
